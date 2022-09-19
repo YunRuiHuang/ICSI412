08c9c5e37f0ed1573c000a31e3cca0aba48e0bf6
YunRuiHuang <yunruihuang@gmail.com>
2022-09-19 17:21:19 -0400

    setup 03

---
+++ 03 Prioritize the Scheduler/OSInterface.java
@@ -0,0 +1,9 @@
+public interface OSInterface {
+    int CreateProcess(UserlandProcess myNewProcess, PriorityEnum priority);
+
+    boolean DeleteProcess(int processId);
+
+    void Sleep(int milliseconds);
+    void run();
+}
+
